7257f0ee4ea10402d7509a81cb710b46aa5e8079
LeoProko <leoproko126@gmail.com>
2023-10-24 22:09:40 +0300

    wip

--- hw_asr/datasets/librispeech_dataset.py
+++ hw_asr/datasets/librispeech_dataset.py
@@ -52,14 +52,14 @@ class LibrispeechDataset(BaseDataset):
         super().__init__(index, *args, **kwargs)
 
     def _load_part(self, part):
-        arch_path = self._data_dir / f"{part}.tar.gz"
+        arch_path = self._index_dir / f"{part}.tar.gz"
         print(f"Loading part {part}")
         download_file(URL_LINKS[part], arch_path)
-        shutil.unpack_archive(arch_path, self._data_dir)
-        for fpath in (self._data_dir / "LibriSpeech").iterdir():
-            shutil.move(str(fpath), str(self._data_dir / fpath.name))
+        shutil.unpack_archive(arch_path, self._index_dir)
+        for fpath in (self._index_dir / "LibriSpeech").iterdir():
+            shutil.move(str(fpath), str(self._index_dir / fpath.name))
         os.remove(str(arch_path))
-        shutil.rmtree(str(self._data_dir / "LibriSpeech"))
+        shutil.rmtree(str(self._index_dir / "LibriSpeech"))
 
     def _get_or_load_index(self, part):
         index_path = self._index_dir / f"{part}_index.json"
