0856581c7c0fdb694c61c2a77695fc2c4150ae66
LeoProko <leoproko126@gmail.com>
2024-01-21 20:51:19 +0300

    wip

--- hw_asr/trainer/trainer.py
+++ hw_asr/trainer/trainer.py
@@ -52,7 +52,7 @@ class Trainer(BaseTrainer):
             k: v for k, v in dataloaders.items() if k != "train"
         }
         self.lr_scheduler = lr_scheduler
-        self.log_step = 50
+        self.log_step = 200
 
         self.train_metrics = MetricTracker(
             "loss", "grad norm", *[m.name for m in self.metrics], writer=self.writer
